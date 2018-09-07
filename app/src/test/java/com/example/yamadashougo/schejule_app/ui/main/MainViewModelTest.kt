d4d46ef065e5e2ee8fd424344d80ab32c0013dbe
yshogo <superman199323@gmail.com>
2018-09-07 22:53:22 +0900

    first commit

---
+++ app/src/test/java/com/example/yamadashougo/schejule_app/ui/main/MainViewModelTest.kt
@@ -0,0 +1,8 @@
+package com.example.yamadashougo.schejule_app.ui.main
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MainViewModelTest {
+}
