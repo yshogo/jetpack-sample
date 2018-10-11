bfcfb989eb9922c1e2f638ff09d72b0a86c83e15
yshogo <superman199323@gmail.com>
2018-10-11 17:32:28 +0900

    無駄な変数の削除

--- app/src/main/java/com/example/yamadashougo/schejule_app/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/yamadashougo/schejule_app/ui/map/MapViewModel.kt
@@ -1,11 +1,8 @@
 package com.example.yamadashougo.schejule_app.ui.map
 
 import android.arch.lifecycle.ViewModel
-import android.databinding.ObservableArrayList
 
 class MapViewModel : ViewModel() {
-
-    var items: ObservableArrayList<Map> = ObservableArrayList()
 
     val mapList = MapRepository().getMapList()
 }
